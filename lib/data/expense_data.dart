31e6acb94748664e95ab1ed493c5df33e4dbe10e
Derrick-Mao <derrickmao03@gmail.com>
2024-02-25 19:22:38 -0800

    init data folder, expense data file

---
+++ lib/data/expense_data.dart
@@ -0,0 +1,20 @@
+class ExpenseData {
+  // list of all expenses
+  List<ExpenseItem> overallExpenseList = [];
+
+  // get expense list
+  List<ExpenseItem> getAllExpenseList() {
+    return overallExpenseList;
+  }
+
+  // add new expense
+  void addNewExpense(ExpenseItem newExpense) {
+    overallExpenseList.add(newExpense);
+  }
+
+  // remove expense
+  void removeExpense(ExpenseItem expense) {
+    overallExpenseList.remove(expense);
+  }
+
+}
