8e4d807e3498ce87c865cac74d265872da95708a
Derrick-Mao <derrickmao03@gmail.com>
2024-02-27 02:06:17 -0800

    get weekday name

--- lib/data/expense_data.dart
+++ lib/data/expense_data.dart
@@ -17,4 +17,28 @@ class ExpenseData {
     overallExpenseList.remove(expense);
   }
 
+  // get weekday name
+  String getWeekdayName(DateTime dateTime) {
+    switch (dateTime.weekday) {
+      case 1:
+        return 'Monday';
+      case 2:
+        return 'Tuesday';
+      case 3:
+        return 'Wednesday';
+      case 4:
+        return 'Thursday';
+      case 5:
+        return 'Friday';
+      case 6:
+        return 'Saturday';
+      case 7:
+        return 'Sunday';
+      default:
+        return '';
+    }
+  }
+
+  // expense graph by week
+
 }
