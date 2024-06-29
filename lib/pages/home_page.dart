a56b8a4011771555a407b5f8ec50fcd48ad48ebe
Derrick-Mao <derrickmao03@gmail.com>
2024-06-29 15:30:42 -0700

    init do not merge

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,18 +4,17 @@ import 'package:motionmint/components/expense_tile.dart';
 import 'package:motionmint/data/expense_data.dart';
 import 'package:motionmint/models/expense_item.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:motionmint/authentication/auth.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  final User? user = Auth().currentUser;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  Future<void> signOut() async {
+    await Auth().signOut();
+  }
 
   final String title;
 
